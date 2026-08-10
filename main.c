9e27c1edb1c454a542089aaf738bd84eeb6a3946
Sonja Wilberding <sonjawilberding@Sonjas-MacBook-Air.local>
2026-08-10 14:39:52 -0500

    fixed tokentype printing

--- main.c
+++ main.c
@@ -150,7 +150,7 @@ int main(int argc, char *argv[]) {
     }
     for (int i = 0; i < tokens; i++)
     {
-        printf("[%02d] type=%s text=\"%s\"\n", i, tokentypeToString(tokenArray[i].type), tokenArray[i].text);
+        printf("[%02d] type=%s text=%s", i, tokentypeToString(tokenArray[i].type), tokenArray[i].text);
     }
 
     Parser parser = { .tokens = tokenArray, .tokenCount = tokens, .pos = 0 };
